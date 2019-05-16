dbec2e6b77ac6aa3fddb7a9adca80e1df718e4c3
liabe <liabe@LAZY-PC>
2019-05-17 00:13:33 +0100

    phase4

--- src/dogfight/controller/DogfightController.java
+++ src/dogfight/controller/DogfightController.java
@@ -1,18 +1,21 @@
 package dogfight.controller;
 
+import java.io.IOException;
+
 import dogfight.model.IDogfightModel;
 import dogfight.view.IViewSystem;
 
 public class DogfightController implements IOrderPerformer {
 	
 	private int TIME_SLEEP = 30 ; 
+	private final IDogfightModel dogfightModel ; 
+	private IViewSystem viewSystem;
 	
 	public DogfightController (IDogfightModel dogfightModel)	{
-		
+		this.dogfightModel = dogfightModel ; 
 	}
 	
-	public void orderPerform (UserOrder userOrder)	{
-		
+	public void orderPerform (final UserOrder userOrder)throws IOException	{
 	}
 	
 	public void play () {
--- src/dogfight/view/GraphicsBuilder.java
+++ src/dogfight/view/GraphicsBuilder.java
@@ -12,11 +12,11 @@ import dogfight.model.IMobile;
 
 public class GraphicsBuilder implements IGraphicsBuilder{
 	
-	private IDogfightModel dogfihgtModel; 
+	private IDogfightModel dogfightModel; 
 	private BufferedImage emptySky ; 
 	
-	public GraphicsBuilder (IDogfightModel dogfihgtModel)	{
-		this.dogfihgtModel = dogfihgtModel ; 
+	public GraphicsBuilder (IDogfightModel dogfightModel)	{
+		this.dogfightModel = dogfightModel ; 
 		this.buildEmptySky();
 	}
 
@@ -24,19 +24,19 @@ public class GraphicsBuilder implements IGraphicsBuilder{
 	public void applyModelToGraphics(Graphics graphics, ImageObserver observer) {
 		// TODO Auto-generated method stub
 		graphics.drawImage(this.emptySky, 0, 0, observer);
-		for (IMobile mobile : this.dogfihgtModel.getMobiles()) {
+		for (IMobile mobile : this.dogfightModel.getMobiles()) {
 			this.drawMobile(mobile, graphics, observer);
 		}
 	}
 	
 	public void buildEmptySky() {
-		this.emptySky = new BufferedImage(this.dogfihgtModel.getArea().getDimension().getWidth(), 
-				this.dogfihgtModel.getArea().getDimension().getHeight(), BufferedImage.TYPE_INT_BGR);
+		this.emptySky = new BufferedImage(this.dogfightModel.getArea().getDimension().getWidth(), 
+				this.dogfightModel.getArea().getDimension().getHeight(), BufferedImage.TYPE_INT_BGR);
 		
 		final Graphics2D graphics =(Graphics2D) this.emptySky.getGraphics()	;
 		
-		graphics.drawImage(this.dogfihgtModel.getArea().getImage(),0,0,this.dogfihgtModel.getArea().getDimension().getWidth(), 
-				this.dogfihgtModel.getArea().getDimension().getHeight(), null);
+		graphics.drawImage(this.dogfightModel.getArea().getImage(),0,0,this.dogfightModel.getArea().getDimension().getWidth(), 
+				this.dogfightModel.getArea().getDimension().getHeight(), null);
 		
 	}
 	
@@ -45,30 +45,30 @@ public class GraphicsBuilder implements IGraphicsBuilder{
 		final Graphics graphicsMobile = imageMobile.getGraphics()	;
 		graphicsMobile.drawImage(mobile.getImage(), 0, 0, mobile.getWidth(), mobile.getHeight(), observer) ; 
 		graphics.drawImage(imageMobile, (int) mobile.getPosition().getX(), (int) mobile.getPosition().getY(),observer);
-		final boolean isHorizontalOut = (mobile.getPosition().getX() + mobile.getWidth()) > this.dogfihgtModel.getArea().getDimension().getWidth()	;
-		final boolean isVerticalOut =(mobile.getPosition().getY() + mobile.getHeight()) > this.dogfihgtModel.getArea().getDimension().getHeight()	; 
+		final boolean isHorizontalOut = (mobile.getPosition().getX() + mobile.getWidth()) > this.dogfightModel.getArea().getDimension().getWidth()	;
+		final boolean isVerticalOut =(mobile.getPosition().getY() + mobile.getHeight()) > this.dogfightModel.getArea().getDimension().getHeight()	; 
 		
 		if (isHorizontalOut) {
 			final BufferedImage imageMobileH = imageMobile.getSubimage(
-					(int) this.dogfihgtModel.getArea().getDimension().getWidth() - (int) mobile.getPosition().getX(), 0,
-					(int) (mobile.getWidth() - (int) this.dogfihgtModel.getArea().getDimension().getWidth()) + (int) mobile.getPosition().getX(),
+					(int) this.dogfightModel.getArea().getDimension().getWidth() - (int) mobile.getPosition().getX(), 0,
+					(int) (mobile.getWidth() - (int) this.dogfightModel.getArea().getDimension().getWidth()) + (int) mobile.getPosition().getX(),
 					mobile.getHeight());
 			graphics.drawImage(imageMobileH, 0, (int) mobile.getPosition().getY(), observer);
 		}
 		
 		if (isVerticalOut) {
 			final BufferedImage imageMobileV = imageMobile.getSubimage(0,
-					(int) this.dogfihgtModel.getArea().getDimension().getHeight() - (int) mobile.getPosition().getY(), mobile.getWidth(),
-					(int) ((mobile.getHeight() - this.dogfihgtModel.getArea().getDimension().getHeight()) + mobile.getPosition().getY()));
+					(int) this.dogfightModel.getArea().getDimension().getHeight() - (int) mobile.getPosition().getY(), mobile.getWidth(),
+					(int) ((mobile.getHeight() - this.dogfightModel.getArea().getDimension().getHeight()) + mobile.getPosition().getY()));
 			graphics.drawImage(imageMobileV, (int) mobile.getPosition().getX(), 0, observer);
 		}
 		
 		if (isHorizontalOut && isVerticalOut) {
 			final BufferedImage imageMobileHV = imageMobile.getSubimage(
-					(int) this.dogfihgtModel.getArea().getDimension().getWidth() - (int) mobile.getPosition().getX(),
-					(int) this.dogfihgtModel.getArea().getDimension().getHeight() - (int) mobile.getPosition().getY(),
-					(int) (mobile.getWidth() - this.dogfihgtModel.getArea().getDimension().getWidth()) + (int) mobile.getPosition().getX(),
-					(int) (mobile.getHeight() - this.dogfihgtModel.getArea().getDimension().getHeight()) + (int) mobile.getPosition().getY());
+					(int) this.dogfightModel.getArea().getDimension().getWidth() - (int) mobile.getPosition().getX(),
+					(int) this.dogfightModel.getArea().getDimension().getHeight() - (int) mobile.getPosition().getY(),
+					(int) (mobile.getWidth() - this.dogfightModel.getArea().getDimension().getWidth()) + (int) mobile.getPosition().getX(),
+					(int) (mobile.getHeight() - this.dogfightModel.getArea().getDimension().getHeight()) + (int) mobile.getPosition().getY());
 			graphics.drawImage(imageMobileHV, 0, 0, observer);
 		}
 		
@@ -77,13 +77,13 @@ public class GraphicsBuilder implements IGraphicsBuilder{
 	@Override
 	public int getGlobalWidth() {
 		// TODO Auto-generated method stub
-		return this.dogfihgtModel.getArea().getDimension().getWidth();
+		return this.dogfightModel.getArea().getDimension().getWidth();
 	}
 
 	@Override
 	public int getGlobalHeight() {
 		// TODO Auto-generated method stub
-		return this.dogfihgtModel.getArea().getDimension().getHeight()	;
+		return this.dogfightModel.getArea().getDimension().getHeight()	;
 	}
 
 }
