2dbef2cdbea88e224582d140dd80474d6ef40c53
liabe <liabe@LAZY-PC>
2019-05-16 21:06:31 +0100

    phase 4

--- src/dogfight/model/IDogfightModel.java
+++ src/dogfight/model/IDogfightModel.java
@@ -1,12 +1,13 @@
 package dogfight.model;
 
+import java.io.IOException;
 import java.util.ArrayList;
 
 public interface IDogfightModel {
 
 	public IArea getArea()	;
 	
-	public void buildArea(Dimension dimension)	;
+	public void buildArea(Dimension dimension) throws IOException	;
 	
 	public void addMobile (Mobile mobile);
 	
